b888d386bc06ff4809ff2533d275ece5db615d49
곽호성 <kakosung@gwaghoseongui-MacBook-Air.local>
2016-06-18 18:14:32 +0900

    a

--- Android_NFCProject/app/src/main/java/kr/ac/kumoh/s20110057/nfcproject/CheckActivity.java
+++ Android_NFCProject/app/src/main/java/kr/ac/kumoh/s20110057/nfcproject/CheckActivity.java
@@ -1,6 +1,7 @@
 package kr.ac.kumoh.s20110057.nfcproject;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -24,33 +25,48 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.BasicNetwork;
 import com.android.volley.toolbox.DiskBasedCache;
 import com.android.volley.toolbox.HurlStack;
+import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.Locale;
 
+import javax.net.ssl.HttpsURLConnection;
+
 public class CheckActivity extends AppCompatActivity {
 
     public static final String LESSONTAG = "LessonTag";
+    protected String id=null;
 
-    protected JSONObject mResult = null;
+    protected JSONArray mResult = null;
 
     protected ArrayList<LessonInfo> mArray = new ArrayList<LessonInfo>();
     protected ListView mList;
     protected LessonAdapter mAdapter;
     protected RequestQueue mQueue = null;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_check);
 
+        Intent intent = getIntent();
+        id = intent.getStringExtra("id");
 
         mAdapter = new LessonAdapter(this, R.layout.lesson_item);
         mList = (ListView)findViewById(R.id.lesson_list);
@@ -59,6 +75,7 @@ public class CheckActivity extends AppCompatActivity {
         Cache cache = new DiskBasedCache(getCacheDir(), 1024 * 1024); // 1MB
         Network network = new BasicNetwork(new HurlStack());
         mQueue = new RequestQueue(cache, network);
+
         mQueue.start();
 
 
@@ -73,18 +90,21 @@ public class CheckActivity extends AppCompatActivity {
         }
     }
 
+    public void NFCTag(){
+
+    }
     //---------------------------------------------------------------------
     public class LessonInfo {
         String name;
-        String date;
+        String code;
         String classNo;
         String teacher;
 
-        public LessonInfo(String name, String date, String classNo,
+        public LessonInfo(String name, String code, String classNo,
                           String teacher) {
             super();
             this.name = name;
-            this.date = date;
+            this.code = code;
             this.classNo = classNo;
             this.teacher = teacher;
         }
@@ -93,8 +113,8 @@ public class CheckActivity extends AppCompatActivity {
             return name;
         }
 
-        public String getDate() {
-            return date;
+        public String getCode() {
+            return code;
         }
 
         public String getClassNo() {
@@ -108,7 +128,7 @@ public class CheckActivity extends AppCompatActivity {
     //---------------------------------------------------------------------
     static class LessonViewHolder {
         TextView txName;
-        TextView txDate;
+        TextView txCode;
         TextView txClassNo;
         TextView txTeacher;
     }
@@ -135,7 +155,7 @@ public class CheckActivity extends AppCompatActivity {
 
                 viewHolder = new LessonViewHolder();
                 viewHolder.txName = (TextView) v.findViewById(R.id.name);
-                viewHolder.txDate = (TextView) v.findViewById(R.id.date);
+                viewHolder.txCode = (TextView) v.findViewById(R.id.date);
                 viewHolder.txClassNo = (TextView) v.findViewById(R.id.class_number);
                 viewHolder.txTeacher = (TextView) v.findViewById(R.id.teacher);
 
@@ -147,7 +167,7 @@ public class CheckActivity extends AppCompatActivity {
             LessonInfo info = mArray.get(position);
             if (info != null) {
                 viewHolder.txName.setText(info.getName());
-                viewHolder.txDate.setText(info.getDate());
+                viewHolder.txCode.setText(info.getCode());
                 viewHolder.txClassNo.setText(info.getClassNo());
                 viewHolder.txTeacher.setText(info.getTeacher());
             }
@@ -156,55 +176,48 @@ public class CheckActivity extends AppCompatActivity {
     }
     //---------------------------------------------------------------------
 
-
     //--------------------------------------------------------------
     protected void requestLesson()
     {
-        String url ="http:";       // json 받아올 url
-
-        JsonObjectRequest jsObjRequest = new JsonObjectRequest(Request.Method.GET, url, null,
-                new Response.Listener<JSONObject>() {
-                    @Override
-                    public void onResponse(JSONObject response) {
-                        mResult = response;
+        String url="http://172.20.10.3:8080/project/getLessonList?id=" + id;
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(url, new Response.Listener<JSONArray> () {
+                 @Override
+                 public void onResponse(JSONArray response) {
+                        mResult=response;
                         drawList();
-                    }
-                },
-                new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        Toast.makeText(CheckActivity.this, "서버 에러", Toast.LENGTH_LONG).show();
-                    }
-                }
-        );
-        jsObjRequest.setTag(LESSONTAG);
-        mQueue.add(jsObjRequest);
+                     }
+             }, new Response.ErrorListener() {
+                 @Override
+                 public void onErrorResponse(VolleyError error) {
+                     Toast.makeText(CheckActivity.this,"서버 에러", Toast.LENGTH_LONG);
+                     }
+             });
+        jsonArrayRequest.setTag(LESSONTAG);
+        mQueue.add(jsonArrayRequest);
     }
     //--------------------------------------------------------------
     public void drawList() {
         mArray.clear();
         try {
-            JSONArray jsonMainNode = mResult.getJSONArray("list");
+            for (int i = 0; i < mResult.length(); i++) {
+                JSONObject jsonChildNode = mResult.getJSONObject(i);
+                String name = jsonChildNode.getString("lessonName");
+                String code = jsonChildNode.getString("lessonCode");
+                String classNo = jsonChildNode.getString("classNo");
+                String teacher = jsonChildNode.getString("teacher");
 
-            for (int i = 0; i < jsonMainNode.length(); i++) {
-                JSONObject jsonChildNode = jsonMainNode.getJSONObject(i);
-
-                JSONObject jsonLessonNode = jsonChildNode.getJSONArray("lesson").getJSONObject(0);
-                String name = jsonLessonNode.getString("LessonName");
-                String classNo = jsonLessonNode.getString("ClassNo");
-                String teacher = jsonLessonNode.getString("Teacher");
-
-                String gmt = jsonChildNode.getString("LessonDate");
+                /*String gmt = jsonChildNode.getString("LessonDate");
                 Date local = new Date(Long.parseLong(gmt) * 1000);
                 String date = new SimpleDateFormat("yyyy-MM-dd", Locale.KOREA).format(local);
-                Log.i("Lesson", " " + name + "(" + classNo + ")");
+                Log.i("Lesson", " " + name + "(" + classNo + ")");*/
 
-                mArray.add(new LessonInfo(name, date, classNo, teacher));
+                mArray.add(new LessonInfo(name, code, classNo, teacher));
             }
         } catch (JSONException | NullPointerException e) {
             Toast.makeText(getApplicationContext(), "Error" + e.toString(),Toast.LENGTH_LONG).show();
             mResult = null;
         }
+
 
         mAdapter.notifyDataSetChanged();
     }
